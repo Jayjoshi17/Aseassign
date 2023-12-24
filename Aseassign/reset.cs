11a1fb2adffe80c2e1bd19cad2837906f2d8caef
Jayjoshi17 <jay420067@gmail.com>
2023-12-24 19:21:32 +0000

    Here, I made this reset class to make the functions starts again easily.

---
+++ Aseassign/reset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+   public class reset
+    {
+        public reset(String commandline, String multiline)
+        {
+            commandline = " ";
+            multiline = " ";
+        }
+        public String do_reset()
+        {
+            return " ";
+        }
+    }
+}
