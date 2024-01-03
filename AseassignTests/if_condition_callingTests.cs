476f50236ee4af08b859d2c119aa7d5d966c8d3a
Jayjoshi17 <jay420067@gmail.com>
2024-01-03 15:55:39 +0000

    In this class also it is just like that I created in previous class that was if_condition_calling just like that in this call with four parameters.

---
+++ AseassignTests/if_condition_callingTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class if_condition_callingTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void if_condition_calling()
+        {
+            if_condition_calling icc = new if_condition_calling(null, null, null, null, null);
+            icc.comm();
+        }
+    }
+}
