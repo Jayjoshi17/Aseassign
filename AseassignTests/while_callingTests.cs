abf2ecd5a7e288de077b85073da93150c743d224
Jayjoshi17 <jay420067@gmail.com>
2024-01-03 20:13:03 +0000

    This is a while loop class in which I made a unit testing successfuly.

---
+++ AseassignTests/while_callingTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class while_callingTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void while_callingTest()
+        {
+            while_calling wc = new while_calling(null, null, null, null, null);
+            wc.cmds();
+        }
+    }
+}
