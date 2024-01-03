c8f6092b3f53c06d655ed3711e0905d612010755
Jayjoshi17 <jay420067@gmail.com>
2024-01-03 20:25:21 +0000

    This is for the while loop for the iteration but it is with only three parameters inti.

---
+++ AseassignTests/while_iterationTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class while_iterationTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void while_iterationTest()
+        {
+            while_iteration wi = new while_iteration(null, null, null);
+            wi.iterate();
+        }
+    }
+}
