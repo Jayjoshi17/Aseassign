7298ae6c41752508b8c905632a0af0ed041edd45
Jayjoshi17 <jay420067@gmail.com>
2024-01-03 16:58:03 +0000

    Here, I made a unit test of method_calling_with_paramtresTest class with passing five parameters.

---
+++ AseassignTests/method_calling_with_paramtresTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class method_calling_with_paramtresTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void method_calling_with_paramtresTest()
+        {
+            method_calling_with_paramtres mp = new method_calling_with_paramtres(null, null, null, null, null);
+            mp.cmds();
+        }
+    }
+}
