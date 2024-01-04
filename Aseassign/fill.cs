98a5865325ca1da0f52ba58e2c87f9e726380195
Jayjoshi17 <jay420067@gmail.com>
2024-01-04 15:01:29 +0000

    This is the unit test of my main code class which I named as multiline class.

--- Aseassign/fill.cs
+++ Aseassign/fill.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,13 @@ namespace Aseassign
 {
     public class fill
     {
+        public fill(Graphics j) 
+        {
+            if (j is null)
+            {
+                throw new ArgumentNullException(nameof(j));
+            }
+        }
         public string fillvalue = "on";
     }
 }
