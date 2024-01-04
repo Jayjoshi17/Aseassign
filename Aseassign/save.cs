c1568c103a94b672e81e5f7f04a89eaeb9ad343b
Jayjoshi17 <jay420067@gmail.com>
2024-01-04 17:00:18 +0000

    This is the save button class file from which I can save the text file and made it unit test also.

---
+++ Aseassign/save.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    public class save
+    {
+        public save(object sd)
+        {   
+            if(sd is null)
+            {
+                throw new ArgumentNullException(nameof(sd));    
+            }
+        }
+    }
+}
---
+++ AseassignTests/saveTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class saveTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void saveTest()
+        {
+            save sv = new save(null);
+            sv.ToString();
+        }
+    }
+}
