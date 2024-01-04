f5368d9042605dbd4c442142c35c647d1cbbb7c8
Jayjoshi17 <jay420067@gmail.com>
2024-01-04 13:06:33 +0000

    In this code it is for variable operation in which I had taken two parameters and one paramaters in the object calling and this is a unit test that I successed in it.

---
+++ AseassignTests/vb_opTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class vb_opTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void vb_opTest()
+        {
+           vb_op vp = new vb_op(null, null);
+            vp.n_w(null);
+        }
+    }
+}
