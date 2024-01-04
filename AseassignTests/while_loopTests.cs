d0bc8cd7aeb747ca59f8239531cb3a787a3699c1
Jayjoshi17 <jay420067@gmail.com>
2024-01-04 15:14:18 +0000

    This is a while loop unit test with single parameters.

---
+++ AseassignTests/while_loopTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    [TestClass()]
+    public class while_loopTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException), "No exception founded")]
+        public void while_loopTest()
+        {
+            while_loop wl = new while_loop(null);
+            wl.validation();
+        }
+    }
+}
