70b0a1b8b93ab626836d0c442fc34ea4b153aef4
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 19:37:35 +0000

    As I had created reste buttton to reset values so this exception is made for it.

---
+++ Aseassign/resetex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    public class resetex : Exception
+    {
+        public resetex(Graphics j, int ypos)
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("Give valid command of Reset", fr, Brushes.Purple, new Point(0, ypos));
+        }
+    }
+}
