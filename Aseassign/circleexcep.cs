54ce5857181326e56076b9133f2501b29fee6afa
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 14:20:20 +0000

    This is exception class of circle in which if user does not enter proper answer then they will get this line.

---
+++ Aseassign/circleexcep.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    internal class circleexcep : Exception
+    {
+        public circleexcep(Graphics j) 
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("Provide decent parameters of circle ", fr, Brushes.Purple, new Point(50, 50));
+        }
+    }
+    
+    }
+
