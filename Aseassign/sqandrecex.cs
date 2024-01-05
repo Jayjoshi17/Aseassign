5816ed6b884371679e288b81a640758131fe0d7b
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 19:30:46 +0000

    This exception is for rectangle and square both combined.

---
+++ Aseassign/sqandrecex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aseassign
+{
+    public class sqandrecex : Exception
+    {
+        public sqandrecex(Graphics j, int ypos) 
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("provide appropiate parameters to rectangle", fr, Brushes.Purple, new Point(0, ypos));
+
+        }
+    }
+}
