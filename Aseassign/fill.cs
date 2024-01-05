77ddd391556ce0676bf02c39933ce21b4293e0ff
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 14:48:05 +0000

    This is an XML document of fill.cs class file with one constructor.

--- Aseassign/fill.cs
+++ Aseassign/fill.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is a fill class file in which user can fill color in it. 
+    /// </summary>
     public class fill
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="j">This is the parameters of Graphics to draw a shapes</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public fill(Graphics j) 
         {
             if (j is null)
