766c0440da278db233159e3602210b0dc65dcaec
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 19:10:18 +0000

    This is the exception for the invalid commands in multiline class.

---
+++ Aseassign/invalidmulti.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    public class invalidmulti : Exception
+    {
+        public invalidmulti(Graphics j, int line) 
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("please enter a valid command in the line" + (line + 1) + "!", fr, Brushes.Purple, new Point(50, 50));
+        }   
+    }
+}
