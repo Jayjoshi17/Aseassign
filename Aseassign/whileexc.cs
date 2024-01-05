2232c2730c2e8a054ec8ac54af8fdc78183fca5d
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 19:57:35 +0000

    This  exception is the last of  loop which is of while class.

---
+++ Aseassign/whileexc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    public class whileexc : Exception
+    {
+        public whileexc(Graphics j, int ypos)
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("Invalid 'while' command", fr, Brushes.Purple, new Point(0, ypos));
+        }
+    }
+}
