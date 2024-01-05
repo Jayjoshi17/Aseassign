432ddb6eb7bb1ee00905f219c9e8e01da71d2308
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 18:58:59 +0000

    This is an exceptiono of if class where it checks for if conditions.

---
+++ Aseassign/ifexec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    public class ifexec : Exception
+    {
+        public ifexec(Graphics j, int ypos) 
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("Not valid 'if' command", fr, Brushes.Purple, new Point(0, ypos));
+        }
+    }
+}
