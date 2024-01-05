499f4466e85233ba18cc79dd253142014bfa7c03
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 19:52:24 +0000

    This exception is the last shaped exception class of triangle.

---
+++ Aseassign/trianglexc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign
+{
+    public class trianglexc : Exception
+    {
+        public trianglexc(Graphics j, int ypos) 
+        {
+            Font fr = new Font("Arial", 14);
+            j.DrawString("Give vaild command of triangle", fr, Brushes.Purple, new Point(0, ypos));
+        }
+    }
+}
