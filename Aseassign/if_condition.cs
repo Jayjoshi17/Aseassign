d69df6ba9af0044cf669507ee54a3fba7e743689
Jayjoshi17 <jay420067@gmail.com>
2024-01-05 17:25:32 +0000

    In this XML document of if condition I had taken one constructor and one method.

--- Aseassign/if_condition.cs
+++ Aseassign/if_condition.cs
@@ -7,11 +7,20 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is a class of if condition it works in loop
+    /// </summary>
     public class if_condition
     {
         String condition;
         Char[] characters;
         Graphics j;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="j">This is the object of Graphics to draw shapes </param>
+        /// <param name="condition"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public if_condition(Graphics j, String condition)
         {
             if (j is null)
@@ -21,6 +30,10 @@ namespace Aseassign
             this.condition = condition;
             this.j = j;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public string if_statement()
         {
             List<String> list = new List<string>();
