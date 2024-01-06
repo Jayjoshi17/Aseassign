6b43a788b222809deea5dae6c637ac0ba4d49d6d
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 17:29:32 +0000

    This is a unit test of while exception class

---
+++ AseassignTests/whileexcTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is a unit test of while exception class.
+    /// </summary>
+    [TestClass()]
+    public class whileexcTests
+    {
+        /// <summary>
+        /// This passes null for Graphics object which will throw an exception.
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void whileexcTest()
+        {
+            whileexc wc = new whileexc(null, 0);
+
+        }
+    }
+}
