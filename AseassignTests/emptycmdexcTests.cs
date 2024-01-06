7813aef3f474f3e0bdcf431f0a252547e7d98718
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 15:44:07 +0000

    This is a unit test of command line empty exception

---
+++ AseassignTests/emptycmdexcTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is a unit test of command line empty exception
+    /// </summary>
+    [TestClass()]
+    public class emptycmdexcTests
+    {
+        /// <summary>
+        /// This will pass null for Graphic object which will throw an exception
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void emptycmdexcTest()
+        {
+            emptycmdexc ecmd = new emptycmdexc(null, 0);
+        }
+    }
+}
