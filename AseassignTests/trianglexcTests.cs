786d60ea7030c4a3ff3d9e43239cb343f7f15b39
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 17:26:32 +0000

    This is a unit test for triangle exception class

---
+++ AseassignTests/trianglexcTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is a unit test for triangle exception class
+    /// </summary>
+    [TestClass()]
+    public class trianglexcTests
+    {
+        /// <summary>
+        /// This will passes null for Graphics object which will throw an exception.
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void trianglexcTest()
+        {
+            trianglexc tc = new trianglexc(null, 0);
+        }
+    }
+}
