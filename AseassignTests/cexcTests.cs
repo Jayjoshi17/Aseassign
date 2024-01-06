7236026e09783e294877f3ef36b7d712239d58b8
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 15:40:47 +0000

    This is an exception unti test of clear class file.

---
+++ AseassignTests/cexcTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is a unit test of clear exception
+    /// </summary>
+    [TestClass()]
+    public class cexcTests
+    {
+        /// <summary>
+        /// This passes null for Graphics object which will throw an exception.
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void cexcTest()
+        {
+            cexc cc = new cexc(null, 0);
+        }
+    }
+}
