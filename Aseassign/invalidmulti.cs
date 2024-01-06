6f6f782e491c3de193de8f6dd5fa5a439363a5cb
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 22:26:49 +0000

    This is a multiline invallid command XML documentation with one constructor.

--- Aseassign/invalidmulti.cs
+++ Aseassign/invalidmulti.cs
@@ -8,8 +8,16 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is the class for multiline invalid command
+    /// </summary>
     public class invalidmulti : Exception
     {
+        /// <summary>
+        /// In this class file their is constructor of invalidmultiline 
+        /// </summary>
+        /// <param name="j">This is the object of Graphics to draw shapes</param>
+        /// <param name="line">It indicates that which specific line is invalid.</param>
         public invalidmulti(Graphics j, int line) 
         {
             Font fr = new Font("Arial", 14);
