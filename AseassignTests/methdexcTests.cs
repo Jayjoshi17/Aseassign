c4c0845537792e27d1bf4b9834e255038a4438c0
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 16:58:20 +0000

    This unit test is for method exception class file.

---
+++ AseassignTests/methdexcTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This unit test is for method exception class file.
+    /// </summary>
+    [TestClass()]
+    public class methdexcTests
+    {
+        /// <summary>
+        /// This passes null for Graphics object which will throw an exception
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]  
+        public void methdexcTest()
+        {
+            methdexc mtc = new methdexc(null, 0);
+
+        }
+    }
+}
