7bca61ae05c4bfbec09ca40eb6a6fde76674ee49
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 23:56:38 +0000

    In this XML documentation their is a class of reset with one constructor with 2 parameters.

--- Aseassign/reset.cs
+++ Aseassign/reset.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// Thisis the reset class for to reset the values which was entered by users.
+    /// </summary>
    public class reset
     {
+        /// <summary>
+        /// In this class their is a constructor of reset
+        /// </summary>
+        /// <param name="commandline">This indicates the commandline values</param>
+        /// <param name="multiline">It indicates the multiline values</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public reset(String commandline, String multiline)
         {
             if (commandline is null)
