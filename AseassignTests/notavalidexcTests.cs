8ebbe2ba4efea00ea7958a9d56ed3ed21475f899
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 17:08:35 +0000

    This is unit test for not a valid excetion class file.

---
+++ AseassignTests/notavalidexcTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+   /// <summary>
+   /// This is unit test for not a valid excetion class
+   /// </summary>
+    [TestClass()]
+    public class notavalidexcTests
+    {
+        /// <summary>
+        /// This passes null for Graphics object which will throw an exception.
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))] 
+        public void notavalidexcTest()
+        {
+            notavalidexc nav = new notavalidexc(null ,0);
+        }
+    }
+}
