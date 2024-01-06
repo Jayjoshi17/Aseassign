0527ebc9432eb684f8a7896801082f9c04465b09
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 23:17:12 +0000

    This XML documentation is for open button.

--- Aseassign/openbt.cs
+++ Aseassign/openbt.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is for the open button by which the txt file gets open
+    /// </summary>
     public class openbt
     {
+        /// <summary>
+        /// This is a method of open button
+        /// </summary>
+        /// <param name="sd"></param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void open(object sd)
         {
                  if (sd is null)
