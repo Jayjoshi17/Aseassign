166e774c08f32259bd046e8efd92aa4ca2ea8106
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 16:48:12 +0000

    This is unit test of exception for if condtion

---
+++ AseassignTests/ifexecTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is unit test of exception for if condtion
+     /// </summary>
+    [TestClass()]
+    public class ifexecTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]  
+        public void ifexecTest()
+        {
+            ifexec ifec = new ifexec(null, 0);
+            ifec.Source = null;
+        }
+    }
+}
