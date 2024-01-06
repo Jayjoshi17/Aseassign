78f680789ffe829f6fdcfb0c15635bb4097d4842
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 17:23:25 +0000

    This is a unit test for square and rectangle exception class file.

---
+++ AseassignTests/sqandrecexTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is a unit test for square and rectangle class
+    /// </summary>
+    [TestClass()]
+    public class sqandrecexTests
+    {
+        /// <summary>
+        /// This passes null for Graphics object which will throw an exception.
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void sqandrecexTest()
+        {
+            sqandrecex sqr = new sqandrecex(null, 0);
+        }
+    }
+}
