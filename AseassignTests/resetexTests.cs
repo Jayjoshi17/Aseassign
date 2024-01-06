b63945c7ef20efa448e74bb35d40413c7d2c1e26
Jayjoshi17 <jay420067@gmail.com>
2024-01-06 17:15:38 +0000

    This is a unit test for reset exception class

---
+++ AseassignTests/resetexTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aseassign;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aseassign.Tests
+{
+    /// <summary>
+    /// This is a unit test for reset exception class
+    /// </summary>
+    [TestClass()]
+    public class resetexTests
+    {
+        /// <summary>
+        /// Null for Graphics object which will throw an exception.
+        /// </summary>
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void resetexTest()
+        {
+           resetex rs = new resetex(null ,0);
+        }
+    }
+}
