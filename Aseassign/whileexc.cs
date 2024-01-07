8be1ef0927078e7d3cfc7d6999807507ca61ad8d
Jayjoshi17 <jay420067@gmail.com>
2024-01-07 01:08:16 +0000

    An XML documentation of while loop exception with one constructor passing 2 parameters

--- Aseassign/whileexc.cs
+++ Aseassign/whileexc.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is an exception class of while loop
+    /// </summary>
     public class whileexc : Exception
     {
+        /// <summary>
+        /// Their is a constructor of while in below
+        /// </summary>
+        /// <param name="j">This is a object for drawing of Graphics</param>
+        /// <param name="ypos">Idicates y-axis</param>
+        /// <exception cref="ArgumentNullException">An exception that will be thrown whenever the value of graphic is null</exception>
         public whileexc(Graphics j, int ypos)
         {
             if (j is null)
