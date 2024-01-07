a982b58b4c370643e540b6a35dd71a7f69da06b4
Jayjoshi17 <jay420067@gmail.com>
2024-01-07 00:04:51 +0000

    Thier is an exception class of square and rectangle with an XML documentation with one constructor including 2 parameters.

--- Aseassign/sqandrecex.cs
+++ Aseassign/sqandrecex.cs
@@ -8,8 +8,17 @@ using System.Windows.Forms;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This exception class is for square and rectangle 
+    /// </summary>
     public class sqandrecex : Exception
     {
+        /// <summary>
+        /// Their is a constructor of square and rectangle just below it.
+        /// </summary>
+        /// <param name="j">This is the object of Graphics to draw shapes</param>
+        /// <param name="ypos">This is for the y-axis</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public sqandrecex(Graphics j, int ypos) 
         {
             if (j is null)
