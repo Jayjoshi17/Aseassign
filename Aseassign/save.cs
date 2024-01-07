07f37ed240de9242acde4c5e41a9ff1e6d501d1b
Jayjoshi17 <jay420067@gmail.com>
2024-01-07 00:02:11 +0000

    In this XML documentation their is a class of save to text file with one constructor.

--- Aseassign/save.cs
+++ Aseassign/save.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is the class for save the text file
+    /// </summary>
     public class save
     {
+        /// <summary>
+        /// To the below of it their is a constructor of save class.
+        /// </summary>
+        /// <param name="sd">It is the object of save</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public save(object sd)
         {   
             if(sd is null)
