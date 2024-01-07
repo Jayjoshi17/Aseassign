3717f715e6b5cfd8fdcf184a1306c6eb23982bba
Jayjoshi17 <jay420067@gmail.com>
2024-01-07 00:32:38 +0000

    In this XML documentation of triangle their are 2 methods of draw and drawfill with 4 parameters

--- Aseassign/triangle.cs
+++ Aseassign/triangle.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This class is for the triangle 
+    /// </summary>
     public class triangle
     {
+        /// <summary>
+        /// Theri is a constructor of triangle class
+        /// </summary>
+        /// <param name="j">This is the object of Graphics to draw shapes</param>
+        /// <param name="p">This is the object of pen to draw shapes</param>
+        /// <exception cref="ArgumentNullException">An excpetion will be thrown when value ot the graphic variable is null</exception>
         public triangle(Graphics j, Pen p)
         {
             if (j is null)
@@ -16,6 +25,14 @@ namespace Aseassign
                 throw new ArgumentNullException(nameof(j));
             }
         }
+        /// <summary>
+        /// Their is a method of draw to triangle  
+        /// </summary>
+        /// <param name="j">This is the object of Graphics to draw shapes</param>
+        /// <param name="p">This is the object of Graphics to draw shapes</param>
+        /// <param name="xpos">This is for the x-axis</param>
+        /// <param name="ypos">This is for the y-axis</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void draw(Graphics j, Pen p, int xpos, int ypos)
         {
             if (j is null)
@@ -25,6 +42,14 @@ namespace Aseassign
             Point[] points = { new Point(9, 80), new Point(100, 10), new Point(150, 200) };
             j.DrawPolygon(p, points);
         }
+        /// <summary>
+        /// Their is a method of drawfill to triangle that fill the colours in it.
+        /// </summary>
+        /// <param name="j">This is the object of Graphics to draw shapes</param>
+        /// <param name="brush">This is the object of add colours</param>
+        /// <param name="xpos">This is for the x-axis</param>
+        /// <param name="ypos">This is for the y-axis</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public void drawfill(Graphics j, Brush brush, int xpos, int ypos)
         {
             if (j is null)
