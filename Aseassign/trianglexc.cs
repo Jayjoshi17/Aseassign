97b1dc6937969f67881a8585d63193b6efafb1a6
Jayjoshi17 <jay420067@gmail.com>
2024-01-07 00:40:42 +0000

    An XML documentation of trianlge exception

--- Aseassign/trianglexc.cs
+++ Aseassign/trianglexc.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace Aseassign
 {
+    /// <summary>
+    /// This is class of exception of triangle
+    /// </summary>
     public class trianglexc : Exception
     {
+        /// <summary>
+        /// This is an constructor of triangle
+        /// </summary>
+        /// <param name="j">This is a object for drawing of Graphics</param>
+        /// <param name="ypos">This is for y-axis</param>
+        /// <exception cref="ArgumentNullException">An exception that will be thrown whenever the value of graphic is null</exception>
         public trianglexc(Graphics j, int ypos) 
         {
             if (j is null)
